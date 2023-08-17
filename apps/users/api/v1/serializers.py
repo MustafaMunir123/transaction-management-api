48a76010b4bb1fc1a7e01cd06d70c20263c855ae
MustafaMunir123 <mustafamunir10@gmail.com>
2023-08-17 13:41:34 +0500

    feat: add json serialzers for users, transaction & accounts models

--- apps/users/api/v1/serializers.py
+++ apps/users/api/v1/serializers.py
@@ -1,1 +1,15 @@
 from rest_framework import serializers
+from apps.users.models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return CustomUser.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        custom_user = CustomUser.objects.filter(id=instance.id).update(**validated_data)
+        return custom_user
